bc81406d011da597f1ee964ef5b813ebc5b4ebb0
Rodrigo Passos <rodrigoarapassos@gmail.com>
2023-06-14 17:42:28 -0300

    Add basic files and icons for cards

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">Memory Game - Brasileirão 2023
+      <div className='App-logo'></div>
       </header>
     </div>
   );
